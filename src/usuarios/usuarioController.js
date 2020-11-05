67f044332f4afc0621a575c1f6dec52290a2c1f8
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-05 13:07:12 +0100

    Hechas funciones de añadir usuario y obtener uno, listos para probarlos. Se comprueba que los usuarios son correctos previamente. Tarea #20 HU #9

--- src/usuarios/usuarioController.js
+++ src/usuarios/usuarioController.js
@@ -6,11 +6,29 @@ class UsuarioController{
 		usuarios = [];
 	}
 
-	addUsuario(){
-
+	addUsuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
+		if((typeof nombre === 'string') && (typeof apellidos === 'string') && (typeof telefono === 'number') && (typeof dni === 'string') && (typeof fecha_nacimiento.getMonth === 'function') && (typeof username === 'string')){
+			if(dni.lenght == 9){
+				if(!(username in usuarios)){
+					usuario = new Usuario(ombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
+					usuarios.push({
+						key : username,
+						value : usuario
+					});
+				}
+			}
+		}
+		else{
+			throw "Los parámetros para crear el usuario no son correctos";
+		}
 	}
 
 	getUsuario(username){
-
+		if(username in usuarios){
+			return usuarios[username];
+		}
+		else{
+			throw "El usuario no existe";
+		}
 	}
 }
