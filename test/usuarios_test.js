df4b8c38976f881eb189199d6f5e2361520713d7
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-07 17:17:43 +0100

    Cambio el should de los test por el expect de Chai. Me gusta más la sintaxis y es más intuitivo. HU #9

--- test/usuarios_test.js
+++ test/usuarios_test.js
@@ -1,17 +1,13 @@
-var should = require("chai").should;
+var expect = require("chai").expect;
 const Usuario = require('../src/usuarios/usuario.js');
 const UsuarioController = require('../src/usuarios/usuarioController.js');
 
 describe("Test sobre adición de usuarios: ", function() {
 	it("Crear un usuario, añadirlo correctamente y devolverlo por el username", function() {
-		Usuario.should.exist();
-		UsuarioController.should.exist();
-
 		uc = new UsuarioController();
-		uc.addUsuario("Fernando", "Izquierdo Romera", "email@ugr.es", "666666666", "77777777X", new Date(1998, 7, 22), "fernando");
+		uc.addUsuario("Fernando", "Izquierdo Romera", "email@ugr.es", 666666666, "77777777X", new Date(1998, 7, 22), "fernando");
 		usuario = new Usuario("Fernando", "Izquierdo Romera", "email@ugr.es", "666666666", "77777777X", new Date(1998, 7, 22), "fernando");
 		usuario_from_uc = uc.getUsuario("fernando");
-
-		usuario_from_uc.to_string().should.equal(usuario.to_string());
+		expect(usuario_from_uc.to_string()).to.equal(usuario.to_string());
 	})
 });
