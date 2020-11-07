1a3929e015130f9a85e73d670e9576cace562d80
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-07 17:29:22 +0100

    En la clase usuarioController, sacamos la comprobación de tipos correctos a una nueva función. Así se lee mejor el código. HU #9

--- src/usuarios/usuarioController.js
+++ src/usuarios/usuarioController.js
@@ -7,15 +7,22 @@ class UsuarioController{
 	}
 
 	addUsuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
+		if(this.comprobarTipos(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username)){
+			if(!(username in this.usuarios)){
+				var usuario = new Usuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
+				this.usuarios[username] = usuario;
+			}
+			else{
+				throw "Usuario ya existe";
+			}
+		}
+	}
+
+	comprobarTipos(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
+		let correcto = false;
 		if((typeof nombre === 'string') && (typeof apellidos === 'string') && (typeof telefono === 'number') && (typeof dni === 'string') && (typeof fecha_nacimiento.getMonth === 'function') && (typeof username === 'string')){
 			if(dni.length === 9){
-				if(!(username in this.usuarios)){
-					var usuario = new Usuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
-					this.usuarios[username] = usuario;
-				}
-				else{
-					throw "Usuario ya existe";
-				}
+				correcto = true;
 			}
 			else{
 				throw "DNI no es correcto";
@@ -24,6 +31,7 @@ class UsuarioController{
 		else{
 			throw "Los parámetros para crear el usuario no son correctos";
 		}
+		return correcto;
 	}
 
 	getUsuario(username){
