d31632a95c2e292f1a2ca8f6d1f01ef7eba1c3a2
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-07 17:21:13 +0100

    Ya funciona el test de creación y obtención de usuarios. Para ello, he tenido que depurar el usuarioController que tenía varios fallos (mal uso del diccionario, falta de algunas excepciones y en algunas ocasiones me he olvidado del this). HU #9 closes #20

--- src/usuarios/usuarioController.js
+++ src/usuarios/usuarioController.js
@@ -3,19 +3,22 @@ const Usuario = require('./usuario.js');
 class UsuarioController{
 	constructor(){
 		//lo utilizaremos como un diccionario
-		usuarios = [];
+		this.usuarios = [];
 	}
 
 	addUsuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
 		if((typeof nombre === 'string') && (typeof apellidos === 'string') && (typeof telefono === 'number') && (typeof dni === 'string') && (typeof fecha_nacimiento.getMonth === 'function') && (typeof username === 'string')){
-			if(dni.lenght == 9){
-				if(!(username in usuarios)){
-					usuario = new Usuario(ombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
-					usuarios.push({
-						key : username,
-						value : usuario
-					});
+			if(dni.length === 9){
+				if(!(username in this.usuarios)){
+					var usuario = new Usuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
+					this.usuarios[username] = usuario;
 				}
+				else{
+					throw "Usuario ya existe";
+				}
+			}
+			else{
+				throw "DNI no es correcto";
 			}
 		}
 		else{
@@ -24,12 +27,15 @@ class UsuarioController{
 	}
 
 	getUsuario(username){
-		if(username in usuarios){
-			return usuarios[username];
+		if(username in this.usuarios){
+			return this.usuarios[username];
 		}
 		else{
+			console.log(this.usuarios);
 			throw "El usuario no existe";
 		}
 	}
 	
 }
+
+module.exports = UsuarioController;
