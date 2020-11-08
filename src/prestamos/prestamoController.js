9d74feb95882738a5438e4413031eb685a7d6998
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-08 11:14:46 +0100

    Llevados a cabo los métodos de la clase controladora del préstamo y listos para probarlos. Tarea #27 HU #10

--- src/prestamos/prestamoController.js
+++ src/prestamos/prestamoController.js
@@ -8,21 +8,39 @@ class PrestamoController{
 	}
 
 	addPrestamo(id, id_libro, username, fecha_inicio){
-
+		if(this.comprobarTipos(id, id_libro, username, fecha_inicio)){
+			if(!(id in this.prestamos)){
+				var fecha_fin = new Date(fecha_inicio);
+				fecha_fin.setDate(fecha_fin.getDate() + this.rango_prestamo);
+				var prestamo = new Prestamo(id, id_libro, username, fecha_inicio, fecha_fin);
+				this.prestamos[id] = prestamo;
+			}
+			else{
+				throw "Identificador del préstamo ya existe";
+			}
+		}
+		else{
+			throw "Los datos del préstamo no son correctos";
+		}
 	}
 
 	comprobarTipos(id, id_libro, username, fecha_inicio){
-
-	}
-
-	comprobarTipos(id, id_libro, username, fecha_inicio){
-
+		//cuando los microservicios se puedan comunicar, podremos comprobar que existen ciertos usuarios o libros, si tienen alguna penalización..
+		let correcto = false;
+		if((typeof id === 'number') && (typeof id_libro === 'number') && (typeof username === 'string') && (typeof fecha_inicio.getMonth === 'function')){
+			correcto = true;
+		}
+		return correcto;
 	}
 
 	getPrestamo(id){
-
+		if(id in this.prestamos){
+			return this.prestamos[id];
+		}
+		else{
+			throw "El identificador del préstamo no existe";
+		}
 	}
-
 }
 
 module.exports = PrestamoController;
