ed8f56cb46af2a4e48a2586ca9cd0ad3e4ad35e9
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-08 11:09:29 +0100

    Creación del cascarón de la clase controladora con los métodos previstos por el momento. Tarea #27 HU #10

---
+++ src/prestamos/prestamoController.js
@@ -0,0 +1,26 @@
+const Prestamo = require('./prestamo.js');
+
+class PrestamoController{
+	constructor(){
+		this.prestamos = [];
+	}
+
+	addPrestamo(id, id_libro, username, fecha_inicio, fecha_fin){
+
+	}
+
+	comprobarTipos(id, id_libro, username, fecha_inicio, fecha_fin){
+
+	}
+
+	comprobarTipos(id, id_libro, username, fecha_inicio){
+
+	}
+
+	getPrestamo(id){
+		
+	}
+
+}
+
+module.exports = PrestamoController;
