67fc83e5cf0a5c699167346bf75c3f82ee0e0072
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-15 19:46:24 +0000

    Modificado test de creación de libros. Tarea #30 HU #8

--- test/libros_test.js
+++ test/libros_test.js
@@ -5,8 +5,8 @@ const LibroController = require('../src/libros/libroController.js');
 describe("Test sobre adición de libros: ", function() {
 	it("Crear un libro, añadirlo correctamente y devolverlo por el id", function() {
 		lc = new LibroController();
-		lc.addLibro(1, "Los Juegos del Hambre", "Suzanne Collins", 2008, 1, 9780439023481, 374, "Scholastic Corporation", "CIENCIAFICCION");
 		libro = new Libro(1, "Los Juegos del Hambre", "Suzanne Collins", 2008, 1, 9780439023481, 374, "Scholastic Corporation", "CIENCIAFICCION");
+		lc.addLibro(libro);
 		libro_from_lc = lc.getLibro(1);
 		expect(libro_from_lc.to_string()).to.equal(libro.to_string());
 	})
