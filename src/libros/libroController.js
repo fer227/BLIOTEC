7e915380b3c31d4227d283cddd0fbc14de935501
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-15 19:35:18 +0000

    Adaptada la nueva clase controladora de libros. Tarea #30 HU #8

--- src/libros/libroController.js
+++ src/libros/libroController.js
@@ -5,51 +5,19 @@ class LibroController{
 		this.libros = [];
 	}
 
-	addLibro(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero){
-		let ngenero = this.comprobarTipos(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero);
-		if(!(id in this.libros)){
-			var libro = new Libro(id, titulo, autor, anio, edicion, isbn, paginas, editorial, ngenero);
-			this.libros[id] = libro;
+	addLibro(libro){
+		if(libro instanceof Libro){
+			if(!(libro.getId() in this.libros)){
+				this.libros[libro.getId()] = libro;
+			}
+			else{
+				throw "Identificador del libro existente";
+			}
 		}
 		else{
-			throw "Identificador del libro existente";
+			throw "No es un libro";
 		}
-	}
-
-	comprobarTipos(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero){
-		if((typeof id === 'number') && (typeof titulo === 'string') && (typeof paginas === 'number') && (typeof genero === 'string') && (typeof autor === 'string') && (typeof anio === 'number') && (typeof edicion === 'number') && (typeof isbn === 'number')){
-			let ngenero = this.comprobarGenero(genero);
-			return ngenero;
-		}
-		else{
-			throw "Los datos del libro no son correctos";
-		}
-	}
-
-	comprobarGenero(genero){
-		//tratamos el género como un número internamente
-		switch(genero){
-			case "TERROR":
-				return 1;
-			case "CIENCIAFICCION":
-				return 2;
-			case "COMIC":
-				return 3;
-			case "DRAMA":
-				return 4;
-			case "INFANTIL":
-				return 5;
-			case "ROMANTICA":
-				return 6;
-			case "DIDACTICA":
-				return 7;
-			case "HISTORICA":
-				return 8;
-			case "MISTERIO":
-				return 9;
-			default:
-				throw "Género no válido";
-		}
+		
 	}
 
 	getLibro(id){
