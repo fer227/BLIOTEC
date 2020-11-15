26af64d2a75c34c197105d55154407f1d1479eb1
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-15 19:24:21 +0000

    Añadida comprobación de tipos a la clase libro. Tarea #30 HU #8

--- src/libros/libro.js
+++ src/libros/libro.js
@@ -1,5 +1,6 @@
 class Libro{
 	constructor(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero){
+		let ngenero = this.comprobarTipos(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero);
 		this.titulo = titulo;
 		this.id = id;
 		this.autor = autor;
@@ -7,8 +8,44 @@ class Libro{
 		this.edicion = edicion;
 		this.isbn = isbn;
 		this.paginas = paginas;
-		this.genero = genero;
+		this.genero = ngenero;
 		this.editorial = editorial;
+	}
+
+	comprobarTipos(id, titulo, autor, anio, edicion, isbn, paginas, editorial, genero){
+		if((typeof id === 'number') && (typeof titulo === 'string') && (typeof paginas === 'number') && (typeof genero === 'string') && (typeof autor === 'string') && (typeof anio === 'number') && (typeof edicion === 'number') && (typeof isbn === 'number')){
+			let ngenero = this.comprobarGenero(genero);
+			return ngenero;
+		}
+		else{
+			throw "Los datos del libro no son correctos";
+		}
+	}
+
+	comprobarGenero(genero){
+		//tratamos el género como un número internamente
+		switch(genero){
+			case "TERROR":
+				return 1;
+			case "CIENCIAFICCION":
+				return 2;
+			case "COMIC":
+				return 3;
+			case "DRAMA":
+				return 4;
+			case "INFANTIL":
+				return 5;
+			case "ROMANTICA":
+				return 6;
+			case "DIDACTICA":
+				return 7;
+			case "HISTORICA":
+				return 8;
+			case "MISTERIO":
+				return 9;
+			default:
+				throw "Género no válido";
+		}
 	}
 
 	getTitulo(){
