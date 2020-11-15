22a368da76018219d985ddac4f57abddf84c379c
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-15 19:01:16 +0000

    Adaptación del controlador de Usuarios. Tarea #30 HU #9

--- src/usuarios/usuarioController.js
+++ src/usuarios/usuarioController.js
@@ -6,32 +6,15 @@ class UsuarioController{
 		this.usuarios = [];
 	}
 
-	addUsuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
-		if(this.comprobarTipos(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username)){
+	addUsuario(usuario){
+		if(usuario instanceof Usuario){
 			if(!(username in this.usuarios)){
-				var usuario = new Usuario(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username);
 				this.usuarios[username] = usuario;
 			}
 			else{
 				throw "Usuario ya existe";
 			}
 		}
-	}
-
-	comprobarTipos(nombre, apellidos, email, telefono, dni, fecha_nacimiento, username){
-		let correcto = false;
-		if((typeof nombre === 'string') && (typeof apellidos === 'string') && (typeof telefono === 'number') && (typeof dni === 'string') && (typeof fecha_nacimiento.getMonth === 'function') && (typeof username === 'string')){
-			if(dni.length === 9){
-				correcto = true;
-			}
-			else{
-				throw "DNI no es correcto";
-			}
-		}
-		else{
-			throw "Los parámetros para crear el usuario no son correctos";
-		}
-		return correcto;
 	}
 
 	getUsuario(username){
