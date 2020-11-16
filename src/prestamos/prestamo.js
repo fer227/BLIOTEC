8629e958a22a3b73011884f4ed3d5e2ed195af39
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-16 08:35:00 +0000

    Modificada la clase de préstamos. El rango de los préstamos se ha definido como una variable de clase estática. Tarea #30 HU #10

--- src/prestamos/prestamo.js
+++ src/prestamos/prestamo.js
@@ -1,11 +1,28 @@
 class Prestamo{
-	constructor(id, id_libro, username, fecha_inicio, fecha_fin){
-		this.id = id;
-		this.id_libro = id_libro;
-		this.username = username;
-		this.fecha_inicio = fecha_inicio;
-		this.fecha_fin = fecha_fin;
-		this.devuelto = false;
+	static rango_prestamo = 10;
+	constructor(id, id_libro, username, fecha_inicio){
+		if(this.comprobarTipos(id, id_libro, username, fecha_inicio)){
+			this.id = id;
+			this.id_libro = id_libro;
+			this.username = username;
+			this.fecha_inicio = fecha_inicio;
+			this.devuelto = false;
+
+			var fecha_fin = new Date(fecha_inicio);
+			fecha_fin.setDate(fecha_fin.getDate() + rango_prestamo);
+			this.fecha_fin = fecha_fin;
+		}
+	}
+
+	comprobarTipos(id, id_libro, username, fecha_inicio){
+		let correcto = false;
+		if((typeof id === 'number') && (typeof id_libro === 'number') && (typeof username === 'string') && (typeof fecha_inicio.getMonth === 'function')){
+			correcto = true;
+		}
+		else{
+			throw "Los parámetros para crear el préstamo son incorrectos";
+		}
+		return correcto;
 	}
 
 	getID(){
