b68d180e1205dba6f71d787e05ce703c96b23a58
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-16 09:12:36 +0000

    Adaptada la clase controladora de los préstamos. Tarea #30 HU #10

--- src/prestamos/prestamoController.js
+++ src/prestamos/prestamoController.js
@@ -3,34 +3,17 @@ const Prestamo = require('./prestamo.js');
 class PrestamoController{
 	constructor(){
 		this.prestamos = [];
-		//número de días que se da en un préstamo.
-		this.rango_prestamo = 10;
 	}
 
-	addPrestamo(id, id_libro, username, fecha_inicio){
-		if(this.comprobarTipos(id, id_libro, username, fecha_inicio)){
-			if(!(id in this.prestamos)){
-				var fecha_fin = new Date(fecha_inicio);
-				fecha_fin.setDate(fecha_fin.getDate() + this.rango_prestamo);
-				var prestamo = new Prestamo(id, id_libro, username, fecha_inicio, fecha_fin);
-				this.prestamos[id] = prestamo;
+	addPrestamo(prestamo){
+		if(prestamo instanceof Prestamo){
+			if(!(prestamo.getID() in this.prestamos)){
+				this.prestamos[prestamo.getID()] = prestamo;
 			}
 			else{
 				throw "Identificador del préstamo ya existe";
 			}
 		}
-		else{
-			throw "Los datos del préstamo no son correctos";
-		}
-	}
-
-	comprobarTipos(id, id_libro, username, fecha_inicio){
-		//cuando los microservicios se puedan comunicar, podremos comprobar que existen ciertos usuarios o libros, si tienen alguna penalización..
-		let correcto = false;
-		if((typeof id === 'number') && (typeof id_libro === 'number') && (typeof username === 'string') && (typeof fecha_inicio.getMonth === 'function')){
-			correcto = true;
-		}
-		return correcto;
 	}
 
 	getPrestamo(id){
