5cf2b3e876db0396b819e47985b0adc3342e3332
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-17 08:24:15 +0000

    Test de creación de préstamos funcionando. HU #10 Tarea closes #30

--- test/prestamos_test.js
+++ test/prestamos_test.js
@@ -7,10 +7,8 @@ describe("Test sobre adición de préstamos: ", function(){
 		rango = 10;
 		pc = new PrestamoController();
 		var today = new Date();
-		var fin = new Date(today);
-		fin.setDate(fin.getDate() + rango);
-		pc.addPrestamo(0, 11, "fernando", today);
-		prestamo = new Prestamo(0, 11, "fernando", today, fin);
+		prestamo = new Prestamo(0, 11, "fernando", today);
+		pc.addPrestamo(prestamo);
 		prestamo_from_pc = pc.getPrestamo(0);
 		expect(prestamo_from_pc.to_string()).to.equal(prestamo.to_string());
 	})
