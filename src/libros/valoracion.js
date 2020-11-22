19f9f9318149fdd65ab6c4a759ee4928c265de80
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-22 18:08:17 +0000

    Creada una clase valoración. HU #22

---
+++ src/libros/valoracion.js
@@ -0,0 +1,44 @@
+class Valoracion{
+    constructor(isbn, username, nota, resenia){
+        if(this.comprobarTipos(isbn, username, nota, resenia)){
+            this.isbn = isbn;
+            this.username = username;
+            this.nota = nota;
+            this.resenia = resenia;
+        }
+        else{
+            throw "El formato de la valoración es incorrecta";
+        }
+    }
+
+    comprobarTipos(isbn, username, nota, resenia){
+        if((typeof isbn === 'number') && (typeof username === 'string') && (typeof nota === 'number') && (typeof resenia === 'string')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    getISBN(){
+        return this.isbn;
+    }
+
+    getUsername(){
+        return this.username;
+    }
+
+    getNota(){
+        return this.nota;
+    }
+
+    to_string(){
+        var cadena = this.isbn.toString() + ", ";
+        cadena += this.username + ", ";
+        cadena += this.nota.toString() + ", ";
+        cadena += this.resenia + ", ";
+        return cadena
+    }
+}
+
+module.exports = Valoracion;
