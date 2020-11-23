1b32873798424c93b2b24c6cb1ff069cf383f20d
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2020-11-23 15:59:28 +0000

    Eliminando un script innecesario #32

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing Dependencies"
-sudo npm install --global gulp-cli gulp mocha
-npm install
-npm install --only=dev
-echo "you can uninstall doing sudo npm uninstall -g mocha chai gulp gulp-cli"
-echo "now you are able to do gulp <task> like: gulp test"
