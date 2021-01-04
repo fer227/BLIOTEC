d9df96f239c05930a44b92457af6e5aad64c4d3e
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-04 15:07:41 +0000

    Primera ruta GET con Polka. HU #7 Tarea #40

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const Polka = require('polka');
+const { json } = require('body-parser');
 const LibroController = require('./src/libros/libroController.js');
 const Libro = require('./src/libros/libro.js');
 
 libroController = new LibroController();
 app = Polka();
 
-app.listen(3030, err => {
+app
+.use(json())
+.get('/libros/', (req, res) => {
+    //HU1 Consultar el catálogo
+    res.statusCode = 200;
+    res.end(JSON.stringify(libroController.getLibros()));
+})
+.listen(6000, err => {
     if (err) throw err;
-    console.log(`> Running on localhost:${3030}`);
+    console.log(`> Running on localhost:${6000}`);
 });
