0c29a6a56b28ecfa3e5de94d2721017ff7a52aaf
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-05 10:16:32 +0000

    Ahora vamos a probar con Koa.js. Instalamos las dependencias y creamos un esqueleto en el index. #40

--- index.js
+++ index.js
@@ -1,65 +1,16 @@
-const Polka = require('polka');
-const { json } = require('body-parser');
+const Koa = require('koa');
+const Router = require('@koa/router');
+const json = require('koa-json');
 const LibroController = require('./src/libros/libroController.js');
 const Libro = require('./src/libros/libro.js');
 
 libroController = new LibroController();
-app = Polka();
+const app = new Koa();
+const router = new Router();
 
-app
-.use(json())
-.get('/libros/', (req, res) => {
-    //HU1 Consultar el catálogo
-    res.statusCode = 200;
-    res.end(JSON.stringify(libroController.getLibros()));
-})
-.get('/libros/:id', (req, res) => {
-    //HU1 Consultar el catálogo (libro concreto)
-    try{
-        libro = libroController.getLibro(req.params.id);
-        res.statusCode = 200;
-        res.end(JSON.stringify(libro));
-    }catch(err){
-        res.statusCode = 400;
-        res.end(JSON.stringify({msg : err}));
-    }
-})
-.get('/libros/genero/:id', (req, res) => {
-    //HU9 Obtener libros por filtros
-    try{
-        libros = libroController.getLibrosByGenero(parseInt(req.params.id));
-        res.statusCode = 200;
-        res.end(JSON.stringify(libros));
-    }catch(err){
-        res.statusCode = 400;
-        res.end(JSON.stringify({msg : err}));
-    }
-})
-.post('/libros/', (req, res) => {
-    //HU2 Introducir nuevos libros
-    body = req.body;
-    libro = new Libro(body.id, body.titulo, body.autor, body.anio, body.edicion, body.ISBN, body.paginas, body.editorial, body.genero);
-    try{
-        libroController.addLibro(libro);
-        res.statusCode = 201;
-        res.end(JSON.stringify({msg : 'Libro creado con éxito.'}));
-    }catch(err){
-        res.statusCode = 400;
-        res.end(JSON.stringify({msg : err}));
-    }
-})
-.delete('/libros/:id', (req, res) => {
-    //HU2 Gestionar el catálogo (eliminar)
-    try{
-        libroController.deleteLibro(req.params.id);
-        res.statusCode = 200;
-        res.end(JSON.stringify({msg: "Libro eliminado con éxito"}));
-    }catch(err){
-        res.statusCode = 400;
-        res.end(JSON.stringify({msg : err}));
-    }
-})
-.listen(6000, err => {
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(6000, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${6000}`);
 });
