7f08c4a3c15022cee8ed09dd6e7cc920bb99ed79
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-05 11:06:43 +0000

    Añadida primera ruta con Koa (post). Añadidas algunas dependencias necesarias. Tarea #40 HU #8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 const json = require('koa-json');
+const bodyParser = require('koa-bodyparser');
 const LibroController = require('./src/libros/libroController.js');
 const Libro = require('./src/libros/libro.js');
 
@@ -8,6 +9,22 @@ libroController = new LibroController();
 const app = new Koa();
 const router = new Router();
 
+//HU2 Introducir nuevos libros
+router.post('/libros/', (ctx) => {
+    body = ctx.request.body;
+    libro = new Libro(body.id, body.titulo, body.autor, body.anio, body.edicion, body.ISBN, body.paginas, body.editorial, body.genero);
+    try{
+        libroController.addLibro(libro);
+        ctx.status = 201;
+        ctx.body = {msg : 'Libro creado con éxito.'};
+    }catch(err){
+        ctx.status = 400;
+        ctx.body = {msg : err};
+    }
+});
+
+app.use(bodyParser());
+app.use(json());
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(6000, err => {
