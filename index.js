7ee656acabc6d3d1c8c4b9e9fabc6d579f6f8b05
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-07 15:48:59 +0000

    Añadidas las rutas de los préstamos con el index principal. Tarea #40

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const json = require('koa-json');
 const bodyParser = require('koa-bodyparser');
 const libros = require('./routes/libros.js');
+const prestamos = require('./routes/prestamos.js');
 
 const app = new Koa();
 
@@ -9,6 +10,8 @@ app.use(bodyParser());
 app.use(json());
 app.use(libros.routes());
 app.use(libros.allowedMethods());
+app.use(prestamos.routes());
+app.use(prestamos.allowedMethods());
 app.listen(6000, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${6000}`);
