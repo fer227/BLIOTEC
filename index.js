3a409b607ab64756e8534d4c6bba684dec766e3a
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-07 19:07:04 +0000

    [skip ci] Añadadidas las rutas de los usuarios al index principal. Tara #40

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const json = require('koa-json');
 const bodyParser = require('koa-bodyparser');
 const libros = require('./routes/libros.js');
 const prestamos = require('./routes/prestamos.js');
+const usuarios = require('./routes/usuarios.js');
 
 const app = new Koa();
 
@@ -12,6 +13,8 @@ app.use(libros.routes());
 app.use(libros.allowedMethods());
 app.use(prestamos.routes());
 app.use(prestamos.allowedMethods());
+app.use(usuarios.routes());
+app.use(usuarios.allowedMethods());
 app.listen(6000, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${6000}`);
