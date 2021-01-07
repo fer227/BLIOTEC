16edcc84bbc6ed09cdff0cb1df1302366c7d7263
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-07 15:46:40 +0000

    Creado el fichero para las rutas de los prestamos con un POST para crearlos. Tarea #40 HU #10

---
+++ routes/prestamos.js
@@ -0,0 +1,32 @@
+const Router = require('@koa/router');
+const Prestamo = require('../src/prestamos/prestamo.js');
+const PrestamoController = require('../src/prestamos/prestamoController');
+
+prestamoController = new PrestamoController();
+const router = new Router();
+
+//HU4 Poder hacer un préstamo
+router.post('/prestamos/', (ctx) => {
+    body = ctx.request.body;
+    fecha_str = body.fecha_inicio;
+    fecha_arr = fecha_str.split('/');
+    if(fecha_arr.length == 3){
+        date = new Date(fecha_arr[2], fecha_arr[1] - 1, fecha_arr[0]);
+        prestamo = new Prestamo(body.id, body.id_libro, body.username, date);
+        try{
+            prestamoController.addPrestamo(prestamo);
+            ctx.status = 201;
+            ctx.body = {msg : 'Préstamo llevado a cabo con éxito.'};
+        }catch(err){
+            ctx.status = 400;
+            ctx.body = {msg : err};
+        }
+    }
+    else{
+        ctx.status = 400;
+        ctx.body = {msg : 'Formato de la fecha incorrecta.'};
+    }    
+});
+
+
+module.exports = router;
