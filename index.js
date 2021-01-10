7d5ca2b6348da6414e452fc8ae38611c7c20ef34
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-10 16:36:53 +0000

    Añadido logger al index principal. #43

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const libros = require('./routes/libros.js');
 const prestamos = require('./routes/prestamos.js');
 const usuarios = require('./routes/usuarios.js');
 const dotenv = require('dotenv').config();
+const logger = require('koa-pino-logger');
 var port = process.env.PORT || 6000;
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(json());
+app.use(logger());
 app.use(libros.routes());
 app.use(libros.allowedMethods());
 app.use(prestamos.routes());
