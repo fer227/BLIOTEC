244e59b2db01e5344ab219724b3aa2b9bf962a2b
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-12 11:20:13 +0000

    Testeo de las rutas sobre creación de usuarios. Tarea #44 HU #9

---
+++ test/api_usuarios_test.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+const server = require('../index.js');
+
+chai.use(chaiHttp);
+
+u1 = {
+    "nombre" : "Fernando",
+    "apellidos": "Izquierdo Romera",
+    "email": "email@gmail.com",
+    "telefono": 666555444,
+    "dni": "75943988Y",
+    "fecha_nacimiento": "22/6/1998",
+    "username": "fernando"
+};
+
+//HU3 Usuario quiere registrarse
+describe("Test sobre las rutas de adición de usuarios", function(){
+    it("Añade un usuario correctamente", function(done){
+        chai.request(server)
+            .post("/usuarios/")
+            .send(u1)
+            .end(function(err, res){
+                expect(res).to.have.status(201);
+                done();
+            });
+    });
+    it("Obtenemos error si intentamos crear otro usuario con el mismo username", function(done){
+        chai.request(server)
+            .post("/usuarios/")
+            .send(u1)
+            .end(function(err, res){
+                expect(res).to.have.status(409);
+                done();
+            });
+    });
+});
